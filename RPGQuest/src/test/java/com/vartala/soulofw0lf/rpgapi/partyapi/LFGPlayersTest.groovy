0ffe5f2e23e15dd8fd53968ad2158d9bed21eda5
soulofw0lf <web_wizardz@mail.com>
2013-06-04 12:33:46 -0500

    - Liscense update

---
+++ RPGQuest/src/test/java/com/vartala/soulofw0lf/rpgapi/partyapi/LFGPlayersTest.groovy
@@ -0,0 +1,25 @@
+package com.vartala.soulofw0lf.rpgapi.partyapi
+
+/**
+ * Created by: soulof
+ * Date: 6/4/13
+ * Time: 3:29 AM
+ *
+This file is part of the Rpg Suite Created by Soulofw0lf and Linksy.
+
+    The Rpg Suite is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    The Rpg Suite is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with The Rpg Suite Plugin you have downloaded.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+class LFGPlayersTest {
+}
